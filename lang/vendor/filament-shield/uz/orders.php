a2df9f79298ffa6753a70c1cc786bcf718820d8f
asadbek-fayzulloev <fayzulloevasadbek@gmail.com>
2026-07-22 08:59:42 +0500

    f

---
+++ lang/vendor/filament-shield/uz/orders.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'not_found' => 'Buyurtma topilmadi!',
+    'updated' => 'Buyurtma yangilandi!',
+    'deleted' => 'Buyurtma o`chirildi!',
+    'created' => 'Buyurtma yaratildi!',
+
+];
